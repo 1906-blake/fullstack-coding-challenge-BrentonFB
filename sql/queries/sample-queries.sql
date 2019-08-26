5039f4b6378769627f54756e50c9853fcaee9ebd
BrentonFB <fosterbyrdbrenton@gmail.com>
2019-08-26 10:07:19 -0400

    added the sql information

---
+++ sql/queries/sample-queries.sql
@@ -0,0 +1,2 @@
+SELECT * FROM grocery_items
+INNER JOIN grocery_list USING (list_id);
---
+++ sql/tables/table-info.sql
@@ -0,0 +1,15 @@
+CREATE TABLE grocery_list (
+	list_id SERIAL PRIMARY KEY NOT NULL,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE grocery_types (
+	type TEXT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE grocery_items (
+	list_id INTEGER REFERENCES grocery_list(list_id),
+	item_name TEXT NOT NULL,
+    amount INTEGER NOT NULL,
+	item_type TEXT NOT NULL REFERENCES grocery_types(type)
+);
